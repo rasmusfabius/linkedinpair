18b070aa934a0a8cf7bf7888b2f5664ef3da45a6
rasmusfabius <rasmusfabius@gmail.com>
2019-12-04 11:01:30 +0100

    Make the card.

--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Jumbotron, Row, Col } from 'reactstrap';
 import ExperienceModal from './ExperienceModal';
 import Api from '../Api';
+import Moment from 'react-moment';
 
 class Experiences extends Component {
   state = {
@@ -31,11 +32,21 @@ class Experiences extends Component {
           <Row>
             <Col>
               <ul>
-                {this.state.experiences.map(exp => (
-                  <li>
-                    {exp.role} at {exp.company} in {exp.area} {exp.startDate} to {exp.endDate}
-                  </li>
-                ))}
+                <div classNames='card'>
+                  <h5 className='card-header'>Featured</h5>
+                  <div className='card-body'>
+                    <h5 className='card-title'>Special title treatment</h5>
+                    {this.state.experiences.map(exp => (
+                      <>
+                        <p className='card-text'>
+                          {exp.role} at {exp.company} in {exp.area} - {exp.startDate} to {exp.endDate} + {exp._id}
+                          <button onClick=''>EDIT</button>
+                          <button>DELETE</button>
+                        </p>
+                      </>
+                    ))}
+                  </div>
+                </div>
               </ul>
             </Col>
           </Row>
