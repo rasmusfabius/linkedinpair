64c525fbd48ce698dd1df30ec84d3d7dfc4f1953
rasmusfabius <rasmusfabius@gmail.com>
2019-12-04 09:58:48 +0100

    Add and post fixed

--- src/Components/ExperienceModal.jsx
+++ src/Components/ExperienceModal.jsx
@@ -1,117 +1,114 @@
-import React, {useState} from 'react';
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
-import {Col, Row, Form, FormGroup, Label, Input} from 'reactstrap';
-import Api from "../Api";
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Col, Row, Form, FormGroup, Label, Input } from 'reactstrap';
+import Api from '../Api';
 
 class ExperienceModal extends React.Component {
+  //const {buttonLabel, className} = props;
+  experience = JSON.parse(JSON.stringify(this.props.experience));
+  state = { modal: false };
 
+  toggle() {
+    this.setState({ modal: !this.state.modal });
+  }
 
-    //const {buttonLabel, className} = props;
-    experience = JSON.parse(JSON.stringify(this.props.experience));
-    state = {modal: false};
+  submit() {
+    const role = document.getElementById('role').value;
+    const company = document.getElementById('company').value;
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+    const description = document.getElementById('description').value;
+    const area = document.getElementById('area').value;
 
+    const experience = { startDate, endDate, description, role, company, area };
+    Api.fetch('/profile/user13/experiences', 'POST', JSON.stringify(experience)).then(() => {
+      this.toggle();
+      window.location.reload();
+    });
+  }
 
-    toggle() {
-        this.setState({ modal: !this.state.modal});
-    }
-
-    submit() {
-        const startDate = document.getElementById("startDate").value;
-        const endDate = document.getElementById("endDate").value;
-        const role = document.getElementById("role").value;
-        const company = document.getElementById("company").value;
-        const area = document.getElementById("area").value;
-
-        const experience = {startDate, role, company, area};
-        Api.fetch("/profile/user13/experiences", "POST", JSON.stringify(experience)).then(() => {
-            this.toggle();
-            //window.location.reload();
-        });
-    };
-
-    render() {
-        const props = this.props;
-        return <>
-            <Button onClick={this.toggle.bind(this)} className={'buttonExpEdit'}>
-                Add new experience
+  render() {
+    const props = this.props;
+    return (
+      <>
+        <Button onClick={this.toggle.bind(this)} className={'buttonExpEdit'}>
+          Add new experience
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)} className={this.props.className}>
+          <ModalHeader toggle={this.toggle.bind(this)}>Edit experience</ModalHeader>
+          <ModalBody>
+            <Form>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='title'>Start date *</Label>
+                    <Input
+                      type='date'
+                      name='startDate'
+                      id='startDate'
+                      defaultValue={props.experience.startDate}
+                      placeholder='Full-stack Developer'
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='title'>End date *</Label>
+                    <Input
+                      type='date'
+                      name='endDate'
+                      id='endDate'
+                      defaultValue={props.experience.endDate}
+                      placeholder='Full-stack Developer'
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='employment'>Employment type</Label>
+                    <Input type='text' name='role' id='role' defaultValue={props.experience.role} placeholder='-' />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='company'>Company *</Label>
+                    <Input type='company' name='email' id='company' defaultValue={props.experience.company} placeholder='Eg: Google LLC' />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='location'>Location</Label>
+                    <Input type='text' name='area' id='area' defaultValue={props.experience.location} placeholder='Bangalore' />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for='description'>Description</Label>
+                    <Input type='text' name='description' id='description' defaultValue={props.experience.description} placeholder='' />
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color='primary' onClick={this.submit.bind(this)}>
+              Add
             </Button>
-            <Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)} className={this.props.className}>
-                <ModalHeader toggle={this.toggle.bind(this)}>Edit experience</ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Label for='title'>Start date *</Label>
-                                    <Input type='date' name='startDate' id='startDate' defaultValue={props.experience.startDate}
-                                           placeholder='Full-stack Developer'/>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Label for='title'>End date *</Label>
-                                    <Input type='date' name='endDate' id='endDate' defaultValue={props.experience.endDate}
-                                           placeholder='Full-stack Developer'/>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Label for='employment'>Employment type</Label>
-                                    <Input type='text' name='role' id='role'
-                                           defaultValue={props.experience.role}
-                                           placeholder='-'/>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Label for='company'>Company *</Label>
-                                    <Input type='company' name='email' id='company'
-                                           defaultValue={props.experience.company}
-                                           placeholder='Eg: Google LLC'/>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Label for='location'>Location</Label>
-                                    <Input type='text' name='area' id='area'
-                                           defaultValue={props.experience.location}
-                                           placeholder='Bangalore'/>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            defaultChecked={props.experience.isChecked}
-                                        />
-                                        <span> Check Me!</span>
-                                    </label>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color='primary' onClick={this.submit.bind(this)}>
-                        Edit
-                    </Button>
-                </ModalFooter>
-            </Modal>
-        </>
-
-    }
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default ExperienceModal;
--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -1,47 +1,48 @@
-import React, {Component} from 'react';
-import {Container, Jumbotron, Row, Col} from "reactstrap";
-import ExperienceModal from './ExperienceModal'
-import Api from "../Api";
+import React, { Component } from 'react';
+import { Container, Jumbotron, Row, Col } from 'reactstrap';
+import ExperienceModal from './ExperienceModal';
+import Api from '../Api';
 
 class Experiences extends Component {
-    state = {
-        experiences: null
-    };
+  state = {
+    experiences: null
+  };
 
-    async loadData() {
-        this.setState({
-            experiences: await Api.fetch('/profile/user13/experiences')
-        });
-    }
+  async loadData() {
+    this.setState({
+      experiences: await Api.fetch('/profile/user13/experiences')
+    });
+  }
 
-    componentDidMount = async () => {
-        this.loadData();
-    };
+  componentDidMount = async () => {
+    this.loadData();
+  };
 
-    render() {
-        if (!this.state.experiences)
-            return null;
-        return (
-
-            <Container>
-                <Jumbotron>
-                    <Row>
-                        <Col>
-                            <ExperienceModal experience={{}}/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <ul>
-                                {this.state.experiences.map((exp) => <li>{exp.role} at {exp.company} in {exp.area}</li>)}
-                            </ul>
-                        </Col>
-                    </Row>
-                </Jumbotron>
-            </Container>
-
-        );
-    }
+  render() {
+    if (!this.state.experiences) return null;
+    return (
+      <Container>
+        <Jumbotron>
+          <Row>
+            <Col>
+              <ExperienceModal experience={{}} />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ul>
+                {this.state.experiences.map(exp => (
+                  <li>
+                    {exp.role} at {exp.company} in {exp.area} {exp.startDate} to {exp.endDate}
+                  </li>
+                ))}
+              </ul>
+            </Col>
+          </Row>
+        </Jumbotron>
+      </Container>
+    );
+  }
 }
 
 export default Experiences;
